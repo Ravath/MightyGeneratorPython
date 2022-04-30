5267290ae4102e46becdabf5159694de512fa2f4
Ravath <remi.vignal@gmail.com>
2022-04-30 14:22:19 +0200

    Introduction of Title and Label Print classes. Some tuning in the printing strategies.

--- wordgenerator/Print.py
+++ wordgenerator/Print.py
@@ -7,7 +7,7 @@ Created on Fri Apr 15 01:37:14 2022
 
 import io
 import typing
-from wordgenerator.NodeIf import AbsLeafNode
+from wordgenerator.NodeIf import AbsLeafNode, AbsGeneratorNode
 
 # pylint: disable-msg=R0903
 
@@ -39,6 +39,9 @@ def singleton(classe_definie):
 class PrintDelegationIf:
     """A delegation for managing a given string."""
 
+    def __init__(self) :
+        PrintDelegationIf.tabs = 0
+
     @classmethod
     def __subclasshook__(cls, subclass) :
         return (hasattr(subclass, 'do_print') and
@@ -49,13 +52,21 @@ class PrintDelegationIf:
         """Print the given text."""
         raise NotImplementedError(f"In class {type(self).__name__}")
 
+    def end_section(self) :
+        """Print the end of a text section."""
+        raise NotImplementedError(f"In class {type(self).__name__}")
+
 @singleton
 class PrintToConsole(PrintDelegationIf):
     """Print the given text to the console."""
 
     def do_print(self, to_print:str) :
         """Print the given text to the console."""
-        print(to_print)
+        tabs = '\t' * PrintDelegationIf.tabs
+        print(tabs + to_print)
+
+    def end_section(self) :
+        pass
 
 @singleton
 class PrintToBuffer(PrintDelegationIf):
@@ -66,7 +77,13 @@ class PrintToBuffer(PrintDelegationIf):
 
     def do_print(self, to_print:str) :
         """Concatene the given text to the generated string."""
-        self.str_build.write(to_print)
+        tabs = ""
+        if self.str_build.getvalue().endswith('\n') :
+            tabs = '\t' * PrintDelegationIf.tabs
+        self.str_build.write(tabs + to_print)
+
+    def end_section(self) :
+        self.str_build.write('\n')
 
     def get_text(self) -> str :
         """Get the generated string."""
@@ -99,15 +116,42 @@ class PrintNode(AbsLeafNode):
         tab_sign="\t"
         print(f"{tab_sign*tabs}{type(self).__name__} : '{self.text}'")
 
-    # """ Printer Delegate Attribute """
-    # def _get_printer(self):
-    #     return PrintNode._printer
-    # def _set_printer(self, newPrinter):
-    #     if isinstance(newPrinter, PrintDelegationIf) :
-    #         PrintNode._printer = newPrinter
-    #     else : raise TypeError("{} is not an implementation of {}"
-    #                             .format(type(newPrinter), "PrintDelegationIf"))
-    # printer = property(_get_printer, _set_printer)
+class Title(PrintNode) :
+    def __init__(self, title:str, child:AbsGeneratorNode) :
+        PrintNode.__init__(self, title)
+        self.child = child
+
+    def execute(self) :
+        """Print the title, and then
+        execute the children with one tabulation more."""
+        PrintNode.execute(self)
+        PrintDelegationIf.tabs += 1
+        self._printer.end_section()
+        self.child.execute()
+        PrintDelegationIf.tabs -= 1
+        self._printer.end_section()
+
+    def print_node(self, tabs:int = 0) :
+        """Print the node name and printed text."""
+        PrintNode.print_node(self, tabs)
+        self.child.print_node(tabs+1)
+
+class Label(PrintNode) :
+    def __init__(self, label:str, child:AbsGeneratorNode) :
+        PrintNode.__init__(self, label + " : ")
+        self.child = child
+
+    def execute(self) :
+        """Print the label, and then
+        execute the children with one tabulation more."""
+        PrintNode.execute(self)
+        self.child.execute()
+        self._printer.end_section()
+
+    def print_node(self, tabs:int = 0) :
+        """Print the node name and printed text."""
+        PrintNode.print_node(self, tabs)
+        self.child.print_node(tabs+1)
 
 #___________________________________________________#
 #                                                   #
